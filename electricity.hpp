37272956883736021dd160dcf5b46ea5ce548d0d
slosilojn <victor.slosilojn@gmail.com>
2014-06-24 22:52:27 -0300

    Modelling remake
    The model was wrong, so almost 50% of the progress so far is trash.
    Restarting the electric circuit class implementation.

--- electricity.hpp
+++ electricity.hpp
@@ -4,77 +4,67 @@
 #include <complex>
 #include "graph.hpp"
 
-class electrical_component {
+class RLC {
+private:
+    complex<double> conductance;
 public:
-    complex<double> impedance;
-    complex<double> current;
-    complex<double> voltage;
-
-    electrical_component(double ir, double ii):impedance(ir,ii){}
+    RLC(double ir, double ii):conductance(ir,ii){}
 };
 
-class wire : public electrical_component {
+class resistor : public RLC {
+private:
 
-};
-
-class resistor : public electrical_component {
 public:
-    double resistance;
-
-    resistor(double r):resistance(r),electrical_component(r,0){}
+    resistor(double r):RLC(r,0){}
 };
 
-class capacitor : public electrical_component {
+class source {
+private:
+    double amplitude;
+    double frequency;
+    double phase;
 public:
-    double capacitance;
-
+    source(double a, double f, double p):amplitude(a),frequency(f),phase(p){}
 };
 
-class inductor : public electrical_component {
+class voltage_source : source {
+private:
+    double amplitude;
+    double frequency;
+    double phase;
 public:
-    double inductance;
+    voltage_source(double a, double f=0, double p=0):source(a,f,p){}
 };
 
-enum CurrentType {AC,DC};
+class wire {
+
+};
 
 class circuit {
 private:
-    CurrentType current_type;
-    double AC_frequency;
-    graph< complex<double> , electrical_component > graph_circuit;           // Vertices hold their voltage (complex) and edges hold circuit components
+    graph< complex<double> , RLC > RLCs;           //
+    vector< source > sources;
+    vector< wire > wires;
 public:
-    void setAC(double);
-    void setDC();
     int nComponents();
     bool goodGraph();
-    void addComponent(int,int,electrical_component);
+    void addComponent(int,int,RLC);
 };
 
 
 /*===================================================================================================================*/
 
-
-void circuit::setAC(double freq)
-{ AC_frequency = freq; current_type = AC; }
-
-void circuit::setDC()
-{ current_type = DC; }
-
 int circuit::nComponents()
-{ return graph_circuit.nEdges(); }
+{ return RLCs.nEdges(); }
 
 bool circuit::goodGraph()
 {
-    matrix<int> adj2 = graph_circuit.adjacency_matrix();
-    adj2 = adj2*adj2;
-    for(int i=0;i<graph_circuit.nVertices();i++) if(adj2(i,i)<2) return false;
-    return graph_circuit.isConnected();
+
 }
 
-void circuit::addComponent(int a, int b, electrical_component link)
+void circuit::addComponent(int a, int b, RLC link)
 {
-    while(a>=graph_circuit.nVertices() || b>=graph_circuit.nVertices() ) graph_circuit.addVertice();
-    graph_circuit.addEdge(a,b,link);
+
 }
 
 
--- kleg.cpp
+++ kleg.cpp
@@ -10,14 +10,7 @@ int main()
     {
     circuit test;
 
-    test.addComponent(0,1,resistor(1));
-    test.addComponent(1,2,resistor(1));
-    test.addComponent(2,0,resistor(1));
 
-
-    cout << "  -- Circuit Stats -- " << endl << endl;
-    cout << " No of Components: " << test.nComponents() << endl;
-    cout << " Circuit is well connected: " << (test.goodGraph() ? "yes":"no") << endl;
 
 
     }
